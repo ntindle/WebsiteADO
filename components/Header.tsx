96581b8f3b0deb17964900eebcb5872e3f9608e0
Nicholas Tindle <nicktindle@outlook.com>
2021-10-06 22:33:16 +0000

    Add example pages and components

---
+++ components/Header.tsx
@@ -0,0 +1,16 @@
+import NavBar from "./NavBar"
+import Image from 'next/image'
+
+const Header = () => (
+  <header>
+     <Image
+        src="http://jquery-az.com/html/images/banana.jpg"
+        alt="Landscape picture"
+        width={500}
+        height={500}
+      />
+    <NavBar/>
+  </header>
+)
+
+export default Header
